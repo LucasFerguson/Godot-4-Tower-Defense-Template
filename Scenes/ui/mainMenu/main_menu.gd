a22dca5fa4bf4f0d40a3fd9a10ab89602238bcbf
Lucas Ferguson <youremail@yourdomain.com>
2025-10-25 00:26:59 -0500

    Changes to image assets and added some comments to the code

--- Scenes/ui/mainMenu/main_menu.gd
+++ Scenes/ui/mainMenu/main_menu.gd
@@ -5,6 +5,8 @@ var mapSelectContainer : PanelContainer
 func _on_quit_button_pressed():
 	get_tree().quit()
 
+# TODO we will need to edit this button to go to a video intro to the game - Lucas
+
 func _on_start_button_pressed():
 	if not mapSelectContainer:
 		var mscScene := preload("res://Scenes/ui/mainMenu/select_map_container.tscn")
