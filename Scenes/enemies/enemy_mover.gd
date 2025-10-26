435bb31d16d25dae14f5e61ef7a5eed633d4b154
Lucas <lucasferguson16@gmail.com>
2025-10-25 21:56:05 -0500

    Fixed Bug animation

--- Scenes/enemies/enemy_mover.gd
+++ Scenes/enemies/enemy_mover.gd
@@ -4,6 +4,7 @@ var enemy_type := "":
 	set(val):
 		enemy_type = val
 		#$Sprite2D.texture = load(Data.enemies[val]["sprite"])
+		$Sprite2D.play()
 		for stat in Data.enemies[val]["stats"].keys():
 			set(stat, Data.enemies[val]["stats"][stat])
 
